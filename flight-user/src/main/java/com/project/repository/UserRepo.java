89df41a2209dccff50eb01b0534fca3e41edf388
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-04-07 14:39:19 +0530

    updated the user repository

--- flight-user/src/main/java/com/project/repository/UserRepo.java
+++ flight-user/src/main/java/com/project/repository/UserRepo.java
@@ -1,9 +1,13 @@
 package com.project.repository;
 
+import com.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepo extends JpaRepository {
+    Optional<User> findByMobileNumber(String mobileNumber);
 
 }

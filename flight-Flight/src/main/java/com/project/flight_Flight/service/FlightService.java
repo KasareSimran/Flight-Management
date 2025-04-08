73c9417ac7c747a46d2c93b79d6879ea697261d4
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-04-08 17:02:25 +0530

    update service by adding method signture

--- flight-Flight/src/main/java/com/project/flight_Flight/service/FlightService.java
+++ flight-Flight/src/main/java/com/project/flight_Flight/service/FlightService.java
@@ -1,5 +1,15 @@
 package com.project.flight_Flight.service;
 
+import com.project.flight_Flight.model.Flight;
+
+import java.util.List;
+
 public interface FlightService {
 
+    Flight addFlight(Flight flight);
+    Flight updateFlight(Long id,Flight flight) throws Exception;
+    void deleteFlight(Long id);
+    Flight getFlightById(Long id) throws Exception;
+    List<Flight> getAllFlights();
+    List<Flight> searchFlights(String from, String to);
 }

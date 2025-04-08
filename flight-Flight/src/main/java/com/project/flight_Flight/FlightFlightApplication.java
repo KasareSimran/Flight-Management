07445ed926df12bb063c4f078d8b04a8cda67c6c
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-04-08 16:21:35 +0530

    added new microservice for flight

---
+++ flight-Flight/src/main/java/com/project/flight_Flight/FlightFlightApplication.java
@@ -0,0 +1,13 @@
+package com.project.flight_Flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightFlightApplication.class, args);
+	}
+
+}
---
+++ flight-Flight/src/test/java/com/project/flight_Flight/FlightFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.flight_Flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightFlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

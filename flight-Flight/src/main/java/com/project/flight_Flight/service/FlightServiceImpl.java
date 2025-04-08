03a05065ae89d83020e4be15f81f7ac7d5933d17
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-04-08 17:22:18 +0530

    update the implementation for all methods

--- flight-Flight/src/main/java/com/project/flight_Flight/service/FlightServiceImpl.java
+++ flight-Flight/src/main/java/com/project/flight_Flight/service/FlightServiceImpl.java
@@ -1,39 +1,62 @@
 package com.project.flight_Flight.service;
 
 import com.project.flight_Flight.model.Flight;
+import com.project.flight_Flight.repository.FlightRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
 @Service
 public class FlightServiceImpl implements FlightService{
+
+
+    @Autowired
+    private FlightRepo flightRepo;
+
+
     @Override
     public Flight addFlight(Flight flight) {
-        return null;
+        return flightRepo.save(flight);
     }
 
     @Override
     public Flight updateFlight(Long id, Flight flight) throws Exception {
-        return null;
+        Flight existing = flightRepo.findById(id)
+                .orElseThrow(() -> new Exception("Flight not found"));
+
+        existing.setAirlineName(flight.getAirlineName());
+        existing.setFlightNumber(flight.getFlightNumber());
+        existing.setFromLocation(flight.getFromLocation());
+        existing.setToLocation(flight.getToLocation());
+        existing.setDepartureTime(flight.getDepartureTime());
+        existing.setArrivalTime(flight.getArrivalTime());
+        existing.setTotalSeats(flight.getTotalSeats());
+        existing.setAvailableSeats(flight.getAvailableSeats());
+        existing.setPrice(flight.getPrice());
+
+        return flightRepo.save(existing);
     }
 
     @Override
     public void deleteFlight(Long id) {
+        flightRepo.deleteById(id);
 
     }
 
     @Override
     public Flight getFlightById(Long id) throws Exception {
-        return null;
+        return flightRepo.findById(id).orElseThrow(()->new Exception("Flight not found"));
     }
 
     @Override
     public List<Flight> getAllFlights() {
-        return null;
+        return flightRepo.findAll();
     }
 
     @Override
     public List<Flight> searchFlights(String from, String to) {
-        return null;
+        return flightRepo.searchFlights(from, to);
     }
 }

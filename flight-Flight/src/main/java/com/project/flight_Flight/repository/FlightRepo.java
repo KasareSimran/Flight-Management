7813852963f3a0fd8c7b3b6a41b64b146270fc71
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-04-08 17:20:41 +0530

    update repository by adding query for searchflight

--- flight-Flight/src/main/java/com/project/flight_Flight/repository/FlightRepo.java
+++ flight-Flight/src/main/java/com/project/flight_Flight/repository/FlightRepo.java
@@ -2,6 +2,8 @@ package com.project.flight_Flight.repository;
 
 import com.project.flight_Flight.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,8 @@ import java.util.List;
 @Repository
 public interface FlightRepo extends JpaRepository<Flight,Long> {
     List<Flight> findByFromAndTo(String from ,String to);
+
+    @Query("SELECT f FROM Flight f WHERE f.fromLocation = :from AND f.toLocation = :to")
+    List<Flight> searchFlights(@Param("from") String from, @Param("to") String to);
+
 }

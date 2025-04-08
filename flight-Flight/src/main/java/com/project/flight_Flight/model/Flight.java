8fee3bf5d34c19b8e1e8953b0537c8bd9e78b8ff
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-04-08 16:43:33 +0530

    update the flight entity by adding attribute

--- flight-Flight/src/main/java/com/project/flight_Flight/model/Flight.java
+++ flight-Flight/src/main/java/com/project/flight_Flight/model/Flight.java
@@ -1,4 +1,43 @@
 package com.project.flight_Flight.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Flight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String airlineName;
+    private String flightNumber;
+
+    private String fromLocation;
+    private String toLocation;
+
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+
+    private int totalSeats;
+    private int availableSeats;
+
+    private double price;
+
+
+
+
+
 }

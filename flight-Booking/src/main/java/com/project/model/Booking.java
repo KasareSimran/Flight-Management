b9f5e771056c810636001f16ae78fa679c36bd2e
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-04-15 20:13:44 +0530

    added the entity and all attribute to it

--- flight-Booking/src/main/java/com/project/model/Booking.java
+++ flight-Booking/src/main/java/com/project/model/Booking.java
@@ -1,2 +1,39 @@
-package com.project.model;public class Booking {
+package com.project.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Booking {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long UserId;
+    private Long FlightId;
+
+    private int numberOfSeats;
+    private double totalAmount;
+
+    private LocalDateTime bookingTime=LocalDateTime.now();
+
+
+
+
 }

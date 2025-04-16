33dd4c23c964d1ae34478422be1413984a8890e3
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-04-16 18:07:58 +0530

    updated the entity by adding manually getter setter constructor

--- flight-Booking/src/main/java/com/project/model/Booking.java
+++ flight-Booking/src/main/java/com/project/model/Booking.java
@@ -15,8 +15,6 @@ import java.time.LocalDateTime;
 @Entity
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 
 public class Booking {
 
@@ -25,7 +23,7 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long UserId;
+    private Long userId;
     private Long FlightId;
 
     private int numberOfSeats;
@@ -34,6 +32,63 @@ public class Booking {
     private LocalDateTime bookingTime=LocalDateTime.now();
 
 
+    public Booking() {
+    }
 
+    public Booking(Long id, Long userId, Long flightId, int numberOfSeats, double totalAmount, LocalDateTime bookingTime) {
+        this.id = id;
+        this.userId = userId;
+        FlightId = flightId;
+        this.numberOfSeats = numberOfSeats;
+        this.totalAmount = totalAmount;
+        this.bookingTime = bookingTime;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getFlightId() {
+        return FlightId;
+    }
+
+    public void setFlightId(Long flightId) {
+        FlightId = flightId;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public LocalDateTime getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(LocalDateTime bookingTime) {
+        this.bookingTime = bookingTime;
+    }
 }

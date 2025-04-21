f0b0c1f9af35d82982ff25a044c111404d533ecd
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-04-21 18:03:22 +0530

    added this file for constants

---
+++ flight-Booking/src/main/java/com/project/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.project.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "your_secret_key_here";
+    public static final String JWT_HEADER = "Authorization";
+}

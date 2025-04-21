64e7fec12c515bf3037f284e687c4259f4a1f626
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-04-21 18:04:56 +0530

    this is jwt provider cliams

---
+++ flight-Booking/src/main/java/com/project/config/JwtProvider.java
@@ -0,0 +1,27 @@
+package com.project.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtProvider {
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(JwtConstant.SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parser().setSigningKey(JwtConstant.SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}}

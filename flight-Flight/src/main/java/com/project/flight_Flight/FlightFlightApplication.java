d2c07f60ed0602b1e23aef7cf529f0b3d0579bf3
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-04-22 18:14:49 +0530

    added and updated

--- flight-Flight/src/main/java/com/project/flight_Flight/FlightFlightApplication.java
+++ flight-Flight/src/main/java/com/project/flight_Flight/FlightFlightApplication.java
@@ -2,8 +2,10 @@ package com.project.flight_Flight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class FlightFlightApplication {
 
 	public static void main(String[] args) {
